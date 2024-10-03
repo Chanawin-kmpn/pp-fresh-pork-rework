d15d640c024024ccce469100da79cda69ae74f00
Upond <pond_chanawin@hotmail.com>
2024-10-03 20:54:34 +0700

    add: add contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import ContactMethodCard from "@/components/ContactMethodCard";
+import React from "react";
 
 const page = () => {
-	return <div>Contact me</div>;
+  return (
+    <div className="flex flex-col gap-8 px-8 py-4 lg:flex-row lg:px-20 lg:py-12 xl:px-40 xl:py-16">
+      <div className="flex-1">
+        <div className="flex flex-col gap-8">
+          <ContactMethodCard />
+        </div>
+      </div>
+      <div className="flex-1">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10037.4664856741!2d100.24353328876467!3d13.715379501174029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2ebb3413245c3%3A0x3457c3adabe8415c!2z4Lia4Lij4Li04Lip4Lix4LiXIOC4nuC4tS7guJ7guLUuIOC5gOC4n-C4o-C4iuC4nuC4reC4o-C5jOC4hCDguIjguLPguIHguLHguJQ!5e1!3m2!1sth!2sth!4v1727928930017!5m2!1sth!2sth"
+          loading="lazy"
+          className="size-full min-h-80"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ components/ContactMethodCard.tsx
@@ -0,0 +1,59 @@
+import {
+  ChatBubbleLeftRightIcon,
+  AtSymbolIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import React from "react";
+const contactMethods = [
+  {
+    icon: <MapPinIcon className="size-6 text-red-500" />,
+    title: "บริษัท พี.พี. เฟรชพอร์ค",
+    description:
+      "จำกัด 98/6 หมู่ 5 ซอย ราษฏร์สามัคคี 3 ตำบล ท่าข้าม อำเภอ สามพราน จังหวัด นครปฐม 73110",
+  },
+  {
+    icon: <ChatBubbleLeftRightIcon className="size-6 text-blue-500" />,
+    title: "ส่งข้อความ",
+    description: "Line: @ppfreshpork",
+  },
+
+  {
+    icon: <PhoneIcon className="size-6 text-green-500" />,
+    title: "เบอร์โทรศัพท์",
+    description: "085-247-9479, 080-931-3628 ",
+  },
+  {
+    icon: <AtSymbolIcon className="size-6 text-purple-500" />,
+    title: "อีเมล",
+    description: "ppfreshpork@gmail.com",
+  },
+];
+
+const ContactMethodCard = () => {
+  return (
+    <>
+      {contactMethods.map((method) => (
+        <div key={method.title} className="flex items-center gap-4">
+          <div
+            className={clsx("inline-block rounded-full p-4", {
+              "bg-blue-200": method.title === "ส่งข้อความ",
+              "bg-red-200": method.title === "บริษัท พี.พี. เฟรชพอร์ค",
+              "bg-green-200": method.title === "เบอร์โทรศัพท์",
+              "bg-purple-200": method.title === "อีเมล",
+            })}
+          >
+            {method.icon}
+          </div>
+          <div className="space-y-2">
+            <p className="text-large-bold">{method.title}</p>
+            <p className="text-normal">{method.description}</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ContactMethodCard;
